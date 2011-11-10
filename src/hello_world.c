9a7dc0a0d65d09d3543fb534bb4c3f0a961d88e6
Aaron Parker <redbaron148@gmail.com>
2011-11-10 15:56:37 -0600

    Basic commit, generating file structure, including basic serial library

---
+++ src/hello_world.c
@@ -0,0 +1,17 @@
+/***************************************************************************
+*********  This comment area should be filled with a description  
+*********         of your program and what it does.               
+***************************************************************************/
+/* Libraries used in Botball are automatically included, but list any additional includes below */
+/* #defines and constants go below.*/
+
+/* Global variables go below (if you absolutely need them).*/
+
+/*Function prototypes below*/
+
+int main()
+{
+	printf("Hello world");
+}
+
+/*Function definitions go below.*/
