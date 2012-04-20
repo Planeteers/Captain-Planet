9c9c5db063406d795f68ecb149fe350ba437e199
Aaron Parker <redbaron148@gmail.com>
2012-04-20 03:58:21 -0500

    Removed printf statements and added a bit of spacing. I'm using this file in the power points and I'm making it pretty... may not be functional anymore...

--- src/final_test.c
+++ src/final_test.c
@@ -7,19 +7,15 @@ void main()
 	CURRENT = MID;
 	
 	float cycle_start_time;
-	while(!black_button())
+	while(CURRENT_TIME != INVALID_TIME)
 	{
-		//cbc_display_clear();
 		cycle_start_time = CURRENT_TIME;
 		DESTINATION=calculate_destination();
-		printf("Setting Dest to: %d from: %d\n",DESTINATION,CURRENT);
 		move_to(CURRENT,DESTINATION);
 		CURRENT = DESTINATION;
 		CHARGED = dock(CURRENT);
-		printf("dock returned: %d\n", CHARGED);
 		update_source_order(cycle_start_time);
-		printf("SOURCE_ORDER: {%d,%d,%d} @ %f\n\n",SOURCE_ORDER[0],SOURCE_ORDER[1],SOURCE_ORDER[2],		cycle_start_time);
-		sleep(.1);
 	}
+	
 	serializer_disconnect();
 }
